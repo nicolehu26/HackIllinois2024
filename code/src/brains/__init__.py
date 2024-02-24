f5f9b4ed20b4b7d0f08e11000567c08a5327d5f7
mnarayan1 <madhumitas.narayan@gmail.com>
2024-02-24 14:38:57 -0600

    brain import issues

--- code/src/brains/__init__.py
+++ code/src/brains/__init__.py
@@ -1,4 +1,4 @@
-from . import base, human_driver, autonomous
+from . import base, human_driver, autonomous, color_brain
 from typing import TypedDict
 import types
 
@@ -7,10 +7,12 @@ class ModuleTypes(TypedDict):
     autonomous: types.ModuleType
     base: types.ModuleType
     human_driver: types.ModuleType
+    color_brain: types.ModuleType
 
 
 Types: ModuleTypes = {
     "autonomous": autonomous,
     "base": base,
-    "human_driver": human_driver
+    "human_driver": human_driver,
+    "color_brain": color_brain
 }
