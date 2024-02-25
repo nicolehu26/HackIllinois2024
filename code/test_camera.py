bdb9e983348e8474cd465b439a3aacd359e2f6cd
Celina <60200600+cia161@users.noreply.github.com>
2024-02-24 19:10:03 -0600

    trying camera for video feed

--- code/test_camera.py
+++ code/test_camera.py
@@ -3,15 +3,22 @@ import time
 from red import red
 from blue import blue
 from yellow import yellow
+import cv2
+
+
 
 if __name__ == '__main__':
 
     total_seconds = 60
     sample_hz = 10
+    fps = 30
 
     camera = camera_module.Camera({
         "show_preview": True
     })
+
+    video_writer = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), fps, (camera.width, camera.height))
+
     start_time = time.time()
 
     while time.time() - start_time < total_seconds:
@@ -20,5 +27,9 @@ if __name__ == '__main__':
         red(camera)
         yellow(camera)
         blue(camera)
+        frame = camera.capture()  # Capture a frame
+        video_writer.write(frame)
         time.sleep(max(0, 1/sample_hz -
                        (time.time() - start_time)))
+
+    video_writer.write(frame)
